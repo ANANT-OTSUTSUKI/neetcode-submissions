a53f410afa7ddde3aeb134ffd834c7a44183d9f3
ANANT-OTSUTSUKI <anantyadav1006@gmail.com>
2026-07-04 21:25:42 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        stack<int> st;
+        vector<int> nge(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && temp[i]>=temp[st.top()]) st.pop();
+            if(st.empty()) nge[i]=0;
+            else nge[i]=st.top()-i;
+            st.push(i);
+        }
+        return nge;
+    }
+};
